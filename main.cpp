21020082ba7d170d8c3badb316683554ab8e7c3b
minhee71 <54484119+minhee71@users.noreply.github.com>
2019-09-28 02:13:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 
 {
 
-// Declare variables.
+    //*****Declare Variables*****\\
 
     int total_days,
         hotel_string_size,
@@ -22,7 +22,7 @@ int main()
 
     std::string trip_location, 
                 location_output,
-                dollar_sign = "$",
+                currency_symbol = "$",
                 hotel_amount,
                 meal_amount,
                 total_amount,
@@ -30,7 +30,8 @@ int main()
                 meal_output,
                 total_output;                ;    
     
-// Request inputs from user.
+    //*****Request Inputs*****\\
+    
     std::cout << "Business Trip Tracker for MINHEE71 Corp.\n";
     std::cout << "Where was the location of the trip? ";
     std::getline(std::cin, trip_location);
@@ -43,15 +44,15 @@ int main()
     std::cout << std::endl << std::endl;
     
     
-//Background work.
+    //*****Background Work*****\\
     
     //Total of hotel and meal meal_expenses.
     total_expenses = hotel_expenses + meal_expenses;   
     
-    //Concatenate "$" to *_amount strings with to_strings function on *_expenses variables.
-    hotel_amount = dollar_sign + std::to_string(hotel_expenses);
-    meal_amount = dollar_sign + std::to_string(meal_expenses);
-    total_amount = dollar_sign + std::to_string(total_expenses);
+    //Concatenate currency_symbol to *_amount strings with to_strings function on *_expenses variables.
+    hotel_amount = currency_symbol + std::to_string(hotel_expenses);
+    meal_amount = currency_symbol + std::to_string(meal_expenses);
+    total_amount = currency_symbol + std::to_string(total_expenses);
     
     //*_amount strings contained 6 digits after decimal point. This removes the last 4 digits. 
     hotel_string_size = hotel_amount.length() - 4; 
@@ -64,33 +65,32 @@ int main()
     meal_output = meal_amount.substr (0,meal_string_size);
     total_output = total_amount.substr (0,total_string_size);
     
-// Display results for user.
-      std::cout << "Here is a summary of your expenses for your business trip.\n";
-      std::cout << std::endl;
+    //*****Display Outputs*****\\
+    std::cout << "Here is a summary of your expenses for your business trip.\n";
+    std::cout << std::endl;
       
-      // Header rows.
-      std::cout << std::setw(20) << std::left << "Location";
-      std::cout << std::setw(12) << std::right << "Totals Days";
-      std::cout << std::setw(15) << std::right << "Hotel Expenses";
-      std::cout << std::setw(14) << std::right << "Meal Expenses";
-      std::cout << std::setw(15) << std::right << "Total Expenses";
-      std::cout << std::endl;
+    // Header rows.
+    std::cout << std::setw(20) << std::left << "Location";
+    std::cout << std::setw(12) << std::right << "Totals Days";
+    std::cout << std::setw(15) << std::right << "Hotel Expenses";
+    std::cout << std::setw(14) << std::right << "Meal Expenses";
+    std::cout << std::setw(15) << std::right << "Total Expenses";
+    std::cout << std::endl;
       
-      std::cout << std::setw(20) << std::left << "--------------------"; //for Location
-      std::cout << std::setw(12) << std::right << "-----------"; //for Total Days
-      std::cout << std::setw(15) << std::right << "--------------"; //for Hotel Expenses
-      std::cout << std::setw(14) << std::right << "-------------"; //for Meal Expenses
-      std::cout << std::setw(15) << std::right << "--------------"; //for Total Expenses
-      std::cout << std::endl;
+    std::cout << std::setw(20) << std::left << "--------------------"; //for Location
+    std::cout << std::setw(12) << std::right << "-----------"; //for Total Days
+    std::cout << std::setw(15) << std::right << "--------------"; //for Hotel Expenses
+    std::cout << std::setw(14) << std::right << "-------------"; //for Meal Expenses
+    std::cout << std::setw(15) << std::right << "--------------"; //for Total Expenses
+    std::cout << std::endl;
     
-      // Data output row. 
-      std::cout << std::setw(20) << std::left << location_output; 
-      std::cout << std::setw(12) << std::right << total_days;
-      std::cout << std::setw(15) << std::right << hotel_output; 
-      std::cout << std::setw(14) << std::right << meal_output; 
-      std::cout << std::setw(15) << std::right << total_output; 
-
-      std::cout << std::endl;
+    // Data output row. 
+    std::cout << std::setw(20) << std::left << location_output; 
+    std::cout << std::setw(12) << std::right << total_days;
+    std::cout << std::setw(15) << std::right << hotel_output; 
+    std::cout << std::setw(14) << std::right << meal_output; 
+    std::cout << std::setw(15) << std::right << total_output; 
+    std::cout << std::endl;
 
   return 0;
 }
